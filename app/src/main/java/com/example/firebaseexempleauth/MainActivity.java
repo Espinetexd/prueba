f40eaf2ec9793ef7085a96f74f35e152421e6022
2-ASIX <sergioaraandac91@gmail.com>
2021-03-12 18:59:18 +0100

    hkvyfiyfu

--- app/src/main/java/com/example/firebaseexempleauth/MainActivity.java
+++ app/src/main/java/com/example/firebaseexempleauth/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.firebaseexempleauth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mRegistrar,mLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRegistrar= findViewById(R.id.BTN_RegistrarUsuari);
+        mLogin = findViewById(R.id.BTN_LoginUsuari);
+
+        mRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ActivityRegistrarUsuari.class));
+            }
+        });
     }
 }
