b91493c40b510b6adc06f05bec763051d71d62a2
2-ASIX <sergioaraandac91@gmail.com>
2021-03-19 17:42:18 +0100

    LA COSA ESTA QUE HEMOS HECHO

--- app/src/main/java/com/example/firebaseexempleauth/ActivityRegistrarUsuari.java
+++ app/src/main/java/com/example/firebaseexempleauth/ActivityRegistrarUsuari.java
@@ -3,14 +3,19 @@ package com.example.firebaseexempleauth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ActivityRegistrarUsuari extends AppCompatActivity {
 
     private EditText mEmail, mPassword;
     private Button mRegistrar;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,12 +26,33 @@ public class ActivityRegistrarUsuari extends AppCompatActivity {
 
 
         mRegistrar=  findViewById(R.id.BTN_RegistrarUsuari);
+
+        mAuth = FirebaseAuth.getInstance();
         mRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 mRegistrar.setText("Hola");
-            }
+                String email  =mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    mEmail.setError("Error format email.");
+                    mEmail.setFocusable(true);
+                }
+                    else if(password.length()<6){
+
+                        mPassword.setError("Password massa curt");
+                        mPassword.setFocusable(true);
+                    }else{
+                        RegistrarUsuari(email, password);
+                }
+                }
+
         });
+    }
+
+    private void RegistrarUsuari(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email,password);
     }
 }
