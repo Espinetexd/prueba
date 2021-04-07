63572a38675cb899fa737197aed72dea9c2fa714
2-ASIX <davidespitorres@gmail.com>
2021-04-07 16:59:09 +0200

    Buto login cap a ActivityLogin Recuperar password iniciat

--- app/src/main/java/com/example/firebaseexempleauth/ActivityLoginUsuari.java
+++ app/src/main/java/com/example/firebaseexempleauth/ActivityLoginUsuari.java
@@ -1,17 +1,72 @@
 package com.example.firebaseexempleauth;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ActivityLoginUsuari extends AppCompatActivity {
+
+    private TextView mRecuperarPsw;
+    private DialogCarregant dialogCarregant;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_usuari);
+        mRecuperarPsw = findViewById(R.id.RecuperarPassword);
         PrepararActionBar();
+        mRecuperarPsw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MostrarDialogRecuperarPassword();
+            }
+        });
+
+
+
+
+    }
+    private void MostrarDialogRecuperarPassword(){
+        AlertDialog.Builder constructorDialog = new AlertDialog.Builder(this);
+        constructorDialog.setTitle("Recuperar password");
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setPadding(10,10,10,10);
+        final EditText emailET = new EditText(this);
+
+        emailET.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        emailET.setMinEms(20);
+        linearLayout.addView(emailET);
+
+        constructorDialog.setView(linearLayout);
+        constructorDialog.setPositiveButton("Recuperar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String email = emailET.getText().toString().trim();
+                
+                IniciarRecuperacioPassword(email);
+                
+                
+            }
+        });
+        constructorDialog.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        constructorDialog.create().show();
+    }
+
+    private void IniciarRecuperacioPassword(String email) {
     }
 
     private void PrepararActionBar(){
--- app/src/main/java/com/example/firebaseexempleauth/MainActivity.java
+++ app/src/main/java/com/example/firebaseexempleauth/MainActivity.java
@@ -24,5 +24,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, ActivityRegistrarUsuari.class));
             }
         });
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ActivityLoginUsuari.class));
+            }
+        });
     }
 }
